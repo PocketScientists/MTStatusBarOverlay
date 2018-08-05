63b67dc24e678d23737d1e09dc7d8b49bf03a728
Juergen Falb <juergen@falb.at>
2018-08-05 13:15:13 +0200

    Changed project layout

---
+++ MTStatusBarOverlay/MTStatusBarOverlay.h
@@ -0,0 +1,18 @@
+//
+//  MTStatusBarOverlay.h
+//  MTStatusBarOverlay
+//
+//  Created by Juergen Falb on 05.08.18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTStatusBarOverlay.
+FOUNDATION_EXPORT double MTStatusBarOverlayVersionNumber;
+
+//! Project version string for MTStatusBarOverlay.
+FOUNDATION_EXPORT const unsigned char MTStatusBarOverlayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTStatusBarOverlay/PublicHeader.h>
+
+#import <MTStatusBarOverlay/MTStatusBarOverlay.h>
